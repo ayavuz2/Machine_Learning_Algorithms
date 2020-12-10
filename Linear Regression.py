e957254a5394409c65bebd02d49e961c26028b61
ayavuz2 <a.yavuzum@gmail.com>
2020-12-10 13:45:34 +0300

    Testing the linear_model.

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
+import pickle
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -13,4 +17,19 @@ predict = "G3"
 X = np.array(data.drop([predict], 1))
 Y = np.array(data[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+
+print(acc)
+
+print("Coefficient: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
